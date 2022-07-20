88997d3577f53c26ecb9fa543671a65d31f0dcba
jacob moracha <jacobmoracha@gmail.com>
2022-07-20 13:48:26 +0300

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import * as styles from "../components/index.module.css"
+
+const IndexPage = () => (
+  <Layout>
+    <div>Moracha</div>
+  </Layout>
+)
+
+export default IndexPage
